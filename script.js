4d413c591c63b6c9cf29c31420eade6efae253f8
Kiahna Carmack <Kiahna.carmack@gmail.com>
2021-12-05 19:16:30 -0800

    commit to 'main'

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-const form= document.querySelector(".Header");
-form.addEventListener("submit", e => {e.preventDefault();
-const inputVal = input.value;
-});
-const apiKey= "0e0021a41163b5dc5b4c2495efeda20a"
+const button = document.querySelector('.button');
+const inputValue = document.querySelector('.inputValue');
+const name = document.querySelector('.name');
+const temp = document.querySelector('.temp');
+
+button.addEventListener('click', function(){
+fetch('api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=0e0021a41163b5dc5b4c2495efeda20a')
+.then(response => response.json())
+.then(data => console.log(data))
+.catach(err => alert('Please enter a valid City name!☹️'))
+})
